4b67c843ede7d1f72ffa0ac804decbf37ba9d4ba
Renato Davoli <112330702+RenatoDaM@users.noreply.github.com>
2025-05-24 17:56:42 -0300

    Revert "feature: base structure"
    This reverts commit ed86308bd9754a0cb40e6d0d62bc9bdfd436d9fa.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router/router';
 
-const app = createApp(App);
-app.use(router);
 createApp(App).mount('#app')
