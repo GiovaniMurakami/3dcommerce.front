88eae27beec9db4457007cfec7b1e4fe21dca5e1
João Antonio Coimbra Novaes Detore <113216996+JoaoDetore7@users.noreply.github.com>
2025-06-01 14:59:13 -0300

    feat: criação da página do carrinho

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ProductDetailView from '../views/ProductDetailView.vue';
 import HomepageView from '../views/HomepageView.vue';
+import CartView from '../views/CartView.vue';
 
 const routes = [
   { path: '/products/:id', component: ProductDetailView, props: true, name: 'ProductDetailView' },
-  { path: '/home', component: HomepageView, props: true, name: 'HomepageView' }
+  { path: '/home', component: HomepageView, props: true, name: 'HomepageView' },
+  { path: '/cart', component: CartView, props: true, name: 'CartView' }
 ];
 
 const router = createRouter({
